2b1a842210e5d44fe862fe15427cc51ab74063f6
Brandon Ojeda <brandono9192@gmail.com>
2023-06-08 21:07:57 -0400

    Linked style1 to all html, moved profiles to connect.html, updated nav bar

--- javascript/script.js
+++ javascript/script.js
@@ -4,11 +4,13 @@ function organizeData() {
 
     for(let index = 0; index < 20; index++) {
         let temp = {
-            "gender" : data.results[index].gender,
             "name" : data.results[index].name.first + " " + data.results[index].name.last,
-            "photo" : data.results[index].picture.large,
-            "country" : data.results[index].country,
             "email" : data.results[index].email,
+            "photo" : data.results[index].picture.large,
+            "gender" : data.results[index].gender,
+            "country" : data.results[index].country,
+
+
         }
         organizedData.push(temp);
     }
@@ -22,21 +24,21 @@ function addUser() {
     let usersDiv = document.querySelector("#profiles");
 
     for (let user = 0; user < users.length; user++) {
-        // let linebreak1 = document.createElement("br")
+
         let tempImg = document.createElement("img");
         let tempName = document.createElement("p");
         let tempEmail = document.createElement("p");
         let linebreak = document.createElement("br");
         let divider = document.createElement("div")
         
-        
+        divider.className = 'profiles'
 
         tempImg.src = users[user].photo;
         tempName = users[user].name;
         tempGender = users[user].gender;
         tempEmail = users[user].email;
 
-        // usersDiv.append(linebreak1);
+
 
         usersDiv.appendChild(divider)
         usersDiv.append(tempImg);
